90b68579f1a0721c742bc7a50c835762576ef323
ferneynava <ferneynava@gmail.com>
2022-09-15 19:13:30 -0500

    :white_check_mark: [Add] es6 let-const y arrow-function
    [Add] es6 let-const y arrow-function

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,31 @@
+//Acá estamos declarando y asignando el valor 
+var lastName = "David";
+//Acá reasignamos el valor de la variables lastName
+lastName = "Oscar";
+console.log(lastName);
+
+//Declaramos y asignamos 
+let fruit = "Apple";
+//Reasignamos el valor de la variable fruit
+fruit = "Kiwi";
+console.log(fruit);
+
+
+const animal = "Dog";
+//Con const no podemos reasignar un valor
+animal = "Cat";
+console.log(animal);
+
+//Arrow Functions  
+const fruits = () => {
+    if(true){
+        var fruit1  =  "Apple";
+        let fruit2 = "Kiwi";
+        const fruit3 = "Banana"
+    }
+    console.log(fruit1); //function scope 
+    console.log(fruit2); //block scope
+    console.log(fruit3); //block scope
+}
+
+fruits();
---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,13 @@
+function square(num){
+    return num * num;
+}
+
+const square = (num) => {
+    return num * num;
+}
+
+const square = num => num *  num;
+
+console.log(square(4));
+
+

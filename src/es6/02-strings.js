b63d6aae4d82112e800ad0561fdabb55613eeb9a
ferneynava <ferneynava@gmail.com>
2022-09-16 18:24:33 -0500

    :white_check_mark: [Add] es6 strings
    [Add] es6 strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,26 @@
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world + "!";
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+let lorem = "esto es un string \n" + "esto es otra linea";
+
+let lorem2 = `Esta es una frase epica
+    la continuación de esa frase epica. 
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+
+let edad = 25 + 1;
+let vive = "Zipaquira"
+
+let ferney = `Ferney tiene ${edad} años y viven en ${vive}`;
+console.log(ferney);
+
+console.log(`Ferney Nava`);

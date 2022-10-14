3faf958664377bc97ef3321350d7b498408dcac3
ferneynava <ferneynava@gmail.com>
2022-10-14 16:58:30 -0500

    :white_check_mark: [Add] 10-set-add.js
    [Add] 10-set-add.js

---
+++ src/es6/10-set-add.js
@@ -0,0 +1,8 @@
+const list = new Set();
+
+list.add("item 1");
+list.add("item 2").add("item 3");
+
+console.log(list)
+
+// Documentacion https://www.digitalocean.com/community/tutorials/understanding-map-and-set-objects-in-javascript-es 

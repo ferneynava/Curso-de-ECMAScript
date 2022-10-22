a0944554995036dbe48bce48e9a9e12fbc357843
ferneynava <ferneynava@gmail.com>
2022-10-22 09:52:48 -0500

    :white_check_mark: [Add] 01-Object-values.js
    [Add] 01-Object-values.js

---
+++ src/es8/01-object-values.js
@@ -0,0 +1,6 @@
+const countries = { MX: "Mexico", 
+                    CO: "Colombia",         
+                    Cl: "Chile",
+                    PE: "Peru" };
+
+console.log(Object.values(countries));

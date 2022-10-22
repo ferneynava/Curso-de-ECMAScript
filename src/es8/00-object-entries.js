95c6f6a034134374b6b992a377a42dcd7bad92b6
ferneynava <ferneynava@gmail.com>
2022-10-22 09:49:17 -0500

    :white_check_mark: [Add] es8 00-object-entries.js
    [Add] es8 00-object-entries.js

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,7 @@
+const countries = { CO: "Colombia", 
+                    MX: "Mexico", 
+                    CL: "Chile", 
+                    PE: "Peru"
+                    };
+
+console.log(Object.entries(countries));

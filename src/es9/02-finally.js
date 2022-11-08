0d20401fe9b1360cbf8c99470adf6035a096868d
ferneynava <ferneynava@gmail.com>
2022-11-07 21:38:32 -0500

    :white_check_mark: [Agregar] 02-finally
    [Agregar] 02-finally

---
+++ src/es9/02-finally.js
@@ -0,0 +1,13 @@
+const anotherFuncion = () => {
+    return new Promise((resolve, reject) => {
+        if(true){
+            resolve("Hey!!");
+        } else {
+            reject("Whoooops!")
+        }
+    })
+}
+anotherFuncion()
+    .then(response => console.log(response)) //En caso que se ejecute resolve  //then 
+    .catch(err => console.log(err)) //En caso que se ejecute reject  //catch 
+    .finally(() => console.log("Finnaly"));

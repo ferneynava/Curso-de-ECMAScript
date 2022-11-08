ba4922dff9d9c57f53056b1d7c9d36b8175bcf27
ferneynava <ferneynava@gmail.com>
2022-11-07 22:26:39 -0500

    :white_check_mark: [Agregar] 04-async.js
    [Agregar] 04-async.js

--- src/es9/02-finally.js
+++ src/es9/02-finally.js
@@ -11,3 +11,5 @@ anotherFuncion()
     .then(response => console.log(response)) //En caso que se ejecute resolve  //then 
     .catch(err => console.log(err)) //En caso que se ejecute reject  //catch 
     .finally(() => console.log("Finnaly"));
+
+    
---
+++ src/es9/04-async.js
@@ -0,0 +1,20 @@
+async function* anotherGenerator() {
+    yield await Promise.resolve(1);
+    yield await Promise.resolve(2);
+    yield await Promise.resolve(3);
+}
+
+const other = anotherGenerator();
+other.next().then(response => console.log(response.value));
+other.next().then(response => console.log(response.value));
+other.next().then(response => console.log(response.value));
+console.log("Hello");
+
+async function arrayOfNames(array){
+    for await (let value of array){
+        console.log(value);
+    }
+}
+
+const names = arrayOfNames(["Ferney", "Nava", "Trujillo"]);
+console.log("After")

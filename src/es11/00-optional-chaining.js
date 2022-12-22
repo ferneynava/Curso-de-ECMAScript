fe8a6d8d6caa672481d0847ad015829b5ebb57cf
ferneynava <ferneynava@gmail.com>
2022-12-22 15:27:22 -0500

    :white_check_mark: [Add] 00-optional-chaining.js
    [Add] 00-optional-chaining.js

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,13 @@
+const user  = {
+    ferneydev: {
+        country: 'CO'
+    },
+    Erika: {
+        country: 'CO'
+    }
+}
+
+console.log(user.Erika.country);
+console.log(user.Erika.age);
+console.log(user?.Rector?.country);
+

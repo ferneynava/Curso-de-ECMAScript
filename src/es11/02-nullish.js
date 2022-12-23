5f9989cd5e4c9b539a99b2f7991d5cd1b678016f
ferneynava <ferneynava@gmail.com>
2022-12-23 17:55:02 -0500

    :white_check_mark: [Add] 02-nullish.js
    [Add] 02-nullish.js

---
+++ src/es11/02-nullish.js
@@ -0,0 +1,11 @@
+const anotherNumber = null;
+const valiDate = anotherNumber ?? 5;
+console.log(valiDate);
+
+/// OR vs Nullish coalescing
+const id = 0
+const orId = id || "Sin id"
+const nullishId = id ?? "Sin id"
+
+console.log(orId)
+console.log(nullishId)

a60ca1d09c2ac197b6fa2a8700a2eb55c29ee836
ferneynava <ferneynava@gmail.com>
2022-12-28 17:09:01 -0500

    :white_check_mark: [Add] 01-replaceAll.js
    [Add] 01-replaceAll.js

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,8 @@
+const string = "JavaScript es un maravilloso lenguaje de programación"
+
+const replacedString = string.replace("JavaScript", "TypeScript")
+console.log(replacedString)
+
+const mensaje = "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web."
+const newMensaje = mensaje.replaceAll("JavaScript", "Python")
+console.log(newMensaje) //Python es maravilloso, con Python puedo crear el futuro de la web.
